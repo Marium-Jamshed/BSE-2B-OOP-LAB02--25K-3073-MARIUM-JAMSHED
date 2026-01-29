ad6c163a130adeb0f06acf41dbc81fcf30bfaffe
Marium-Jamshed <k253073@nu.edu.pk>
2026-01-29 10:58:16 +0500

    Add TASK4.java to calculate a specific formula

---
+++ TASK4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class lab2{
+
+    public static void main(String[] args) {
+        double result = (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5);
+        System.out.println(result);
+}}
+
